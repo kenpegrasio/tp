7940c2586caaef831a33dd38dfcb9f5ea2250232
adbsw <179715666+adbsw@users.noreply.github.com>
2026-03-12 14:05:13 +0800

    Fix bug to pass ui testing

--- src/main/java/seedu/inventorybro/InventoryBro.java
+++ src/main/java/seedu/inventorybro/InventoryBro.java
@@ -8,6 +8,9 @@ public class InventoryBro {
      * Main entry-point for the java.seedu.inventorybro.InventoryBro application.
      */
     public static void main(String[] args) {
-
+        System.out.println("InventoryBRO");
+        System.out.println("Enter something:");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Test: " + in.nextLine());
     }
 }
