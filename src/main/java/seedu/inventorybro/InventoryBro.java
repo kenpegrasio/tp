4a97a71e5fc5b7ef63c075b8dfab3ccb9fa37334
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2026-03-14 15:47:34 +0800

    feat: character by character reader

--- src/main/java/seedu/inventorybro/InventoryBro.java
+++ src/main/java/seedu/inventorybro/InventoryBro.java
@@ -1,6 +1,6 @@
 package seedu.inventorybro;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class InventoryBro {
 
@@ -9,8 +9,29 @@ public class InventoryBro {
      */
     public static void main(String[] args) {
         System.out.println("InventoryBRO");
-        System.out.println("Enter something:");
-        Scanner in = new Scanner(System.in);
-        System.out.println("Test: " + in.nextLine());
+        System.out.println("How can I help you today, bro?");
+        String currentCommand = new String();
+        while (true) {
+            int ch;
+            try {
+                ch = System.in.read();
+            } catch (IOException e) {
+                System.out.println("Error when parsing characters!");
+                break;
+            }
+            switch (ch) {
+            case '\r':
+                break;
+            case '\n': {
+                System.out.println("Command received: " + currentCommand);
+                currentCommand = new String();
+                break;
+            }
+            default: {
+                currentCommand += (char) ch;
+                break;
+            }
+            }
+        }
     }
 }
