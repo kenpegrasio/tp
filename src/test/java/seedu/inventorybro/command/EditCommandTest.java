f5277dcf432f83f66096d81c7492386aab88be0c
adbsw <179715666+adbsw@users.noreply.github.com>
2026-03-18 05:46:31 +0800

    Fix import statement to pass checkstyle

--- src/test/java/seedu/inventorybro/command/EditCommandTest.java
+++ src/test/java/seedu/inventorybro/command/EditCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.inventorybro.command;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
